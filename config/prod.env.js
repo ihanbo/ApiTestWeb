bf7068c1c487eb006d08190adfad399386f6b965
chencm <chaoming007@163.com>
2019-08-29 16:44:00 +0800

    修改测试系统接口地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,4 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',                 //生产环境
   // BASE_URL: '"http://192.168.15.10:9000/api/"'     // 接口根地址
   BASE_URL: '"http://ms.yiche.com/testapi/api/"'     // 接口根地址
+  //BASE_URL:'"http://test.apptestapi.yiche.com:9000/api/"'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -2,6 +2,7 @@
 module.exports = {
   NODE_ENV: '"test"',                 //测试环境
   //BASE_URL: '"http://192.168.15.10:9000"'     // 接口根地址
-  BASE_URL: '"http://172.20.4.55/testapi/api/"'     // 接口根地址
-  //BASE_URL:'"http://test.apptestapi.yiche.com/api/"'
+  //BASE_URL: '"http://172.20.4.55/testapi/api/"'     // 接口根地址
+  BASE_URL:'"http://192.168.87.148:9000/api/"'
+  
 }
