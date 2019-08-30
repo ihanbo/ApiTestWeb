0e3d08368d255793ef5bc9daf4d54d3f15e7fc8d
zhangjunlei <543453981@qq.com>
2019-08-30 10:06:33 +0800

    no message

---
+++ config/test.env.js
@@ -0,0 +1,7 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"test"',                 //测试环境
+  //BASE_URL: '"http://192.168.15.10:9000"'     // 接口根地址
+  BASE_URL: '"http://172.20.4.55/testapi/api/"'     // 接口根地址
+  //BASE_URL:'"http://test.apptestapi.yiche.com/api/"'
+}
